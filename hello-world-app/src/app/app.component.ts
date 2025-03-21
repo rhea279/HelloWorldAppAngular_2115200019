9fda2b87ab44d4c14d1b569c0455444aebb30d0e
Rhea Lather <rhea.lather_cs.da21@gla.ac.in>
2025-03-21 22:34:42 +0530

    [Rhea] ADD: Added UC4 code to the branch

--- hello-world-app/src/app/app.component.ts
+++ hello-world-app/src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [CommonModule, FormsModule,RouterModule] 
 })
 export class AppComponent {
-  title = 'hello-world-app';
+  userName = ''; 
 
-  // Function to open BridgeLabz URL in a new tab
   openBridgeLabz() {
-    window.open('https://www.bridgelabz.com', '_blank');
+    window.open('https://www.bridgelabz.com', '_blank'); 
   }
 }
