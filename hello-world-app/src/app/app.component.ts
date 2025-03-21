42133148bd3cd39e9c3610d3e8b734659ffef1e8
Rhea Lather <rhea.lather_cs.da21@gla.ac.in>
2025-03-21 21:52:27 +0530

    [Rhea] ADD: Added UC2 code to branch

--- hello-world-app/src/app/app.component.ts
+++ hello-world-app/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  message: string = 'Hello from BridgeLabz';
+  logoUrl: string = 'BL_logo_square_jpg.jpg'; 
 }
